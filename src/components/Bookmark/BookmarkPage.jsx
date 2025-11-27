b6ad4d8eb0aeceacff9ba91631e5aab45adb4526
Raunak Ramdas Bandodcar <raunakhome@Raunaks-MacBook-Pro.local>
2025-11-27 22:23:33 +0000

    Completed article designs

--- src/components/Bookmark/BookmarkPage.jsx
+++ src/components/Bookmark/BookmarkPage.jsx
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "../Bookmark/bookmarkPage.scss";
-import sortIcon from "../../../public/Images/icon-sort.svg";
-import menuBookmark from "../../../public/Images/icon-menu-bookmark.svg";
-import eye from "../../../public/Images/icon-visit-count.svg";
-import clock from "../../../public/Images/icon-last-visited.svg";
-import calender from "../../../public/Images/icon-created.svg";
-import pin from "../../../public/Images/icon-pin.svg";
-import gitHub from "../../../public/Images/icon-pin.svg";
+import sortIcon from "../../assets/Images/icon-sort.svg";
+import menuBookmark from "../../assets/Images/icon-menu-bookmark.svg";
+import eye from "../../assets/Images/icon-visit-count.svg";
+import clock from "../../assets/Images/icon-last-visited.svg";
+import calender from "../../assets/Images/icon-created.svg";
+import pin from "../../assets/Images/icon-pin.svg";
 import { getDate } from "../../constants";
 
 function BookmarkPage() {
   const [data, setData] = useState([]);
-  const [img, setImg] = useState("../../assets/images/favicon-github.png");
 
   const getData = (url) => {
     fetch("src/data.json", {
@@ -44,49 +42,55 @@ function BookmarkPage() {
       <div className="articles">
         {data.map((item, index) => (
           <div key={index} className={`gridArticles articleCard${index + 1}`}>
-            <div className="articleHeader">
-              <img
-                src={`/Images/${item.favicon}`}
-                alt=""
-                className="articleHeaderImage"
-              />
-              <span>
-                <h1 className="articleHeaderText">{item.title}</h1>
-                <span className="articleHeaderTextSecondary">{item.url}</span>
-              </span>
+            <div className="mainArticle">
+              <div className="articleHeader">
+                <img
+                  src={`/Images/${item.favicon}`}
+                  alt=""
+                  className="articleHeaderImage"
+                />
+                <span>
+                  <h1 className="articleHeaderText">{item.title}</h1>
+                  <span className="articleHeaderTextSecondary">{item.url}</span>
+                </span>
 
-              <button className="articleBookmark">
-                <img src={menuBookmark} alt="" />
-              </button>
-            </div>
-            <div className="articleBody">
-              <p className="articleBodyText">{item.description}</p>
-              <div className="articleBodyTags">
-                {item.tags.map((tag) => (
-                  <span className="tagOne">{tag}</span>
-                ))}
+                <button className="articleBookmark">
+                  <img src={menuBookmark} alt="" />
+                </button>
               </div>
-            </div>
-            <div className="articleFooter">
-              <span className="footerIconContainer special">
-                <img src={eye} alt="" className="footerIcon" />
-                <span className="footerText">{item.visitCount}</span>
-              </span>
-              <span className="footerIconContainer">
-                {" "}
-                <img src={clock} alt="" className="footerIcon" />
-                <span className="footerText">{getDate(item.createdAt)}</span>
-              </span>
-              <span className="footerIconContainer">
-                {" "}
-                <img src={calender} alt="" className="footerIcon" />
-                <span className="footerText">{getDate(item.lastVisited)}</span>
-              </span>
-              {item.pinned ? (
-                <img src={pin} alt="" className="footerPin" />
-              ) : (
-                ""
-              )}
+              <div className="articleBody">
+                <div className="articleTextBodyScroll">
+                  <p className="articleBodyText">{item.description}</p>
+                </div>
+                <div className="articleBodyTags">
+                  {item.tags.map((tag) => (
+                    <span className="tagOne">{tag}</span>
+                  ))}
+                </div>
+              </div>
+              <div className="articleFooter">
+                <span className="footerIconContainer special">
+                  <img src={eye} alt="" className="footerIcon" />
+                  <span className="footerText">{item.visitCount}</span>
+                </span>
+                <span className="footerIconContainer">
+                  {" "}
+                  <img src={clock} alt="" className="footerIcon" />
+                  <span className="footerText">{getDate(item.createdAt)}</span>
+                </span>
+                <span className="footerIconContainer">
+                  {" "}
+                  <img src={calender} alt="" className="footerIcon" />
+                  <span className="footerText">
+                    {getDate(item.lastVisited)}
+                  </span>
+                </span>
+                {item.pinned ? (
+                  <img src={pin} alt="" className="footerPin" />
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
           </div>
         ))}
